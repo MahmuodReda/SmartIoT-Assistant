b0e421b20aa79b8b1feecce250c12cd46bd702cb
Mahmood_Reda <mr4141705@gmail.com>
2026-01-11 02:08:24 +0300

    03 add JSON parsing to Communication module with file handling .

--- src/Communication.cpp
+++ src/Communication.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <fstream>
 #include "Communication.hpp"
+#include "json.hpp" // nlohmann JSON
+
+using json = nlohmann::json;
 
 void Communication_Test()
 {
-    std::cout << "[Communication] Module linked successfully" << std::endl;
+    std::ifstream input_file("data/input.json");
+
+    if (!input_file.is_open())
+    {
+        std::cerr << "[COMM] Failed to open input.json\n";
+        return;
+    }
+    json data;
+    try
+    {
+
+        input_file >> data;
+    }
+    catch (...)
+    {
+        std::cerr << "[COMM] Invalid JSON format\n";
+        return;
+    }
+
+    int battery_level = data["battery_level"];
+    bool presence = data["presence"];
+    int steps = data["steps"];
+
+    std::cout << "[COMM] battery_level = " << battery_level << "\n";
+    std::cout << "[COMM] presence      = " << presence << "\n";
+    std::cout << "[COMM] steps         = " << steps << "\n";
+
+    input_file.close();
 }
